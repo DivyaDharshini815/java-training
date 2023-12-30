884e717f8f11fb194778510b87b3a7829b32736c
DivyaDharshini815 <113349739+DivyaDharshini815@users.noreply.github.com>
2023-12-30 22:29:58 +0530

    Add files via upload

---
+++ Printdatatype.java
@@ -0,0 +1,23 @@
+class Printdatatype
+{
+public static void main (String[] args)
+{
+boolean var =true;
+char b ='z';
+byte g =-8;
+short s =-10;
+int c =15;
+long l =20;
+float a =0.5f;
+double k =100.5d;
+
+System.out.println (var);
+System.out.println (b);
+System.out.println (g);
+System.out.println (s);
+System.out.println (c);
+System.out.println (l);
+System.out.println (a);
+System.out.println (k);
+}
+}
