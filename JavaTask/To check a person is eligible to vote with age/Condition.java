4ebe8d34c5f321db04c8520c53b1ce4f67965c8b
DivyaDharshini815 <113349739+DivyaDharshini815@users.noreply.github.com>
2024-01-04 17:07:54 +0530

    Add files via upload

---
+++ JavaTask/To check a person is eligible to vote with age/Condition.java
@@ -0,0 +1,16 @@
+class Condition
+{
+public static void main (String[] args)
+{
+int votingAge = 18;
+int userAge = 24;
+if (userAge >= votingAge)
+{
+System.out.println ("Eligible");
+}
+else 
+{
+System.out.println ("Not Eligible");
+}
+}
+}
