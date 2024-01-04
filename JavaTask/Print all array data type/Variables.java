38b4250785eaba6074ab29c0bae3676824a22b63
DivyaDharshini815 <113349739+DivyaDharshini815@users.noreply.github.com>
2024-01-04 17:22:39 +0530

    Add files via upload

---
+++ JavaTask/Print all array data type/Variables.java
@@ -0,0 +1,41 @@
+class Variables
+{
+public static void main (String[] args)
+{
+boolean var[]=new boolean[2];
+var[0]=true;
+var[1]=false;
+char c[]=new char[2];
+c[0]='b';
+c[1]='p';
+byte num[]=new byte[1];
+num[0]=15;
+short odd[]=new short[2];
+odd[0]=3;
+odd[1]=5;
+int age[]=new int[1];
+age[0]=5;
+long even[]=new long[1];
+even[0]=-20;
+float number[]=new float[2];
+number[0]=10.4f;
+number[1]=10.6f;
+double type[]=new double[1];
+type[0]=100.5d;
+String name[]=new String[1];
+name[0]="Divya";
+String[] StringArray={"almond","dates"};
+System.out.println (var[0]);
+System.out.println (var[1]);
+System.out.println (c[0]);
+System.out.println (c[1]);
+System.out.println (num[0]);
+System.out.println (odd[0]);
+System.out.println (odd[1]);
+System.out.println (age[0]);
+System.out.println (even[0]);
+System.out.println (number[0]);
+System.out.println (number[1]);
+System.out.println (type[0]);
+}
+}
