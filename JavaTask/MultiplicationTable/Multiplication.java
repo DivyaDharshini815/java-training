cf72298b03c9558255fbfc054be370afe76627e8
DivyaDharshini815 <113349739+DivyaDharshini815@users.noreply.github.com>
2024-01-10 20:29:15 +0530

    Add files via upload

---
+++ JavaTask/MultiplicationTable/Multiplication.java
@@ -0,0 +1,28 @@
+class Multiplication
+{
+public static void main(String[] args)
+{
+for(int i=1; i<=10; i++)
+{
+System.out.println("10*" +i+ "=" +10*i);
+}
+System.out.println();
+int j=1;
+while(j<=10)
+{
+System.out.println("2*" +j+ "=" +2*j);
+j++;
+}
+System.out.println();
+int f=1;
+do
+{
+System.out.println("5*" +f+ "=" +5*f);
+f++;
+}
+while(f<=10);
+}
+}
+
+
+
