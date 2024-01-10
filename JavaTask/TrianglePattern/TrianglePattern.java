8af8689e8f5c2ba906cdcc2b6bf0c50d9153d98d
DivyaDharshini815 <113349739+DivyaDharshini815@users.noreply.github.com>
2024-01-10 13:34:55 +0530

    Add files via upload

---
+++ JavaTask/TrianglePattern/TrianglePattern.java
@@ -0,0 +1,16 @@
+class TrianglePattern
+{
+public static void main(String[] args)
+{
+int rows=6;
+for (int i=1; i<=6; i++)
+{
+for (int j=1; j<=i; j++)
+{
+System.out.println("*");
+}
+System.out.println();
+}
+}
+}
+
