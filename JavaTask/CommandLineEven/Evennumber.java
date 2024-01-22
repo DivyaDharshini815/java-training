0afcd84a0ea0b451c1b32e1be9c04651e9cfe84b
DivyaDharshini815 <113349739+DivyaDharshini815@users.noreply.github.com>
2024-01-22 17:26:01 +0530

    Add files via upload

---
+++ JavaTask/CommandLineEven/Evennumber.java
@@ -0,0 +1,15 @@
+class Evennumber
+{
+public static void main(String[] args)
+{
+int number = Integer.parseInt(args[0]);
+if (number %2==0)
+{
+System.out.println("Even number");
+}
+else
+{
+System.out.println("Not Even number");
+}
+}
+}
