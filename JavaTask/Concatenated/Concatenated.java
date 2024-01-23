f5e0fb92a5ad75db39904c93fd974415871761e1
DivyaDharshini815 <113349739+DivyaDharshini815@users.noreply.github.com>
2024-01-23 19:16:31 +0530

    Add files via upload

---
+++ JavaTask/Concatenated/Concatenated.java
@@ -0,0 +1,10 @@
+class Concatenated
+{
+public static void main(String[] args)
+{
+String str1=args[0];
+String str2=args[1];
+String concatStr=str1+str2;
+System.out.println("The Concatinated of given string is "+concatStr);
+}
+}
